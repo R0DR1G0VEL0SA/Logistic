dcf9f7241db6ba5ef6414956226a7342ff9b4b69
jvelosa <jrvelosa@misena.edu.co>
2019-07-07 00:35:17 -0500

    Creación del Proyecto de Muestra
    Empresas

---
+++ Fuentes/empresas/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadChildren: './componentes/login/login.module#LoginPageModule' },
+  { path: 'menu', loadChildren: './componentes/menu/menu.module#MenuPageModule' }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
